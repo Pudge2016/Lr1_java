61ff3a29cef7394f1b0deb2a46600ff54e2f7926
Pudge2016 <160437357+Pudge2016@users.noreply.github.com>
2024-04-19 10:33:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,19 @@
-
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private static volatile int min = Integer.MAX_VALUE;
-
     public static void main(String[] args) {
+        Main main = new Main();
         ArrayManager arrayManager = new ArrayManager(1000, 10);
         arrayManager.generateArray();
-        arrayManager.findMin();
-        System.out.println("Мінімальний елемент масиву: " + min);
-        System.out.println("Індекс мінімального елементу: " + arrayManager.findIndex(min));
+        arrayManager.findMin(main);
+        System.out.println("Мінімальний елемент масиву: " + main.min.get());
+        System.out.println("Індекс мінімального елементу: " + arrayManager.findIndex(main.min.get()));
     }
 
-    public static synchronized void updateMin(int localMin) {
-        if (localMin < min) {
-            min = localMin;
-        }
+    private final AtomicInteger min = new AtomicInteger(Integer.MAX_VALUE);
+
+    public void updateMin(int localMin) {
+        min.updateAndGet(currentMin -> Math.min(currentMin, localMin));
     }
 }
