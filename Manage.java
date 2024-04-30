3c56dc49d6f782d9bcafa6c931f36f94990e7fee
Pudge2016 <160437357+Pudge2016@users.noreply.github.com>
2024-04-30 12:14:20 +0300

    Update Manage.java

--- Manage.java
+++ Manage.java
@@ -1,25 +1,17 @@
+import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
-class Manage {
-    private final Semaphore producerSemaphore;
-    private final Semaphore consumerSemaphore;
-    private final int capacity;
+public class Manager {
 
-    public Manage(int capacity) {
-        this.capacity = capacity;
-        this.producerSemaphore = new Semaphore(capacity);
-        this.consumerSemaphore = new Semaphore(0);
-    }
+    public Semaphore access;
+    public Semaphore full;
+    public Semaphore empty;
 
-    public void produce(int product) throws InterruptedException {
-        producerSemaphore.acquire();
-        System.out.println("Producer produced: " + product);
-        consumerSemaphore.release();
-    }
+    public ArrayList<String> storage = new ArrayList<>();
 
-    public void consume(int product) throws InterruptedException {
-        consumerSemaphore.acquire();
-        System.out.println("Consumer consumed: " + product);
-        producerSemaphore.release();
+    public Manager(int storageSize) {
+        access = new Semaphore(1);
+        full = new Semaphore(storageSize);
+        empty = new Semaphore(0);
     }
 }
